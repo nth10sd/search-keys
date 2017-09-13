f1224235d542f10e408a69d7d413268ca2299f45
Gary Kwong <gary@rumblingedge.com>
2017-09-12 18:06:32 -0700

    Add license header to background.js

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 function getMessage(message) {
   if (message.selection) console.log("FOO");
   // console.log(browser.pageAction.getTitle())
